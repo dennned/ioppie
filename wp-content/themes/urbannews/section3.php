90e7b00758bc40f0e0bbc9a980f58d0c148f5420
makarenkodeniz <makarenkodeniz@gmail.com>
2015-03-01 23:22:41 +0000

    youtube API working...

--- wp-content/themes/urbannews/section3.php
+++ wp-content/themes/urbannews/section3.php
@@ -13,20 +13,133 @@
 			
 			$xmlFeed = isset($data['section3_link']) ? $data['section3_link'] : '';
 			
-			if (($response_xml_data = file_get_contents($xmlFeed))===false){
+			if (($response_xml_data = file_get_contents($xmlFeed))===false) {
 				echo "Error fetching XML\n";
 			} else {
 				$xml = simplexml_load_file($xmlFeed);
 				foreach ($xml->entry as $item) {
-					var_dump((string)$item->title);
+					//var_dump((string)$item->title);
 				}
 			}
+			
+			$xml = new XMLReader();
+			$xml->open('http://gdata.youtube.com/feeds/api/videos/fMoAISMBs7E');
+			
+			$r = array();
+			while($xml->read()) {
+				//$r[] = array($xml->name => $xml->value);
+				
+				//var_dump($xml->name);
+			}
+			
+			//var_dump($r);
+			
+			
+			
+			
+			$v = getYouTubeInfo('fMoAISMBs7E');
+			var_dump($v);
+			
+			
+			
+			
+			// Get & Parse YouTube XML data
+			function getYouTubeInfo($id){
+			
+				//$xml_data = @file_get_contents("http://gdata.youtube.com/feeds/api/videos/".$id);
+				
+				$xml_data = @file_get_contents("http://gdata.youtube.com/feeds/api/playlists/PLMXVftzmkndx4vOOOiA9E397OiiWfCFWY");
+				
+				if(strlen($xml_data) < 25){
+					// Not enough data
+					return $this->getYouTubeInfoBasic($id);
+				}
+				$doc = new DOMDocument();
+				$doc->preserveWhiteSpace = false;
+				if( ! $doc->loadXML($xml_data) ){
+					// Failed Loading XML Doc
+					return $this->getYouTubeInfoBasic($id);
+				}
+			
+				$ret = array();
+			
+				// Get Thumbnail HREFs
+				$media = $doc->getElementsByTagNameNS("*","thumbnail");
+			
+				if($media){
+					foreach($media as $node){
+						$ret["thumbnails"][] = $node->getAttribute("url");
+					}
+				}
+			
+				// Get Video Title
+				$titles = $doc->getElementsByTagNameNS("*","title");
+			
+				if($titles){
+					$ret["title"] = $titles->item(0)->nodeValue;
+				}
+				
+				// Get Description
+				// Get Video Title
+				$description = $doc->getElementsByTagNameNS("*","description");
+				if($description){
+					$ret["description"] = $description->item(0)->nodeValue;
+				}
+				//var_dump($description);
+			
+				// Get Link to Video
+				$link = $doc->getElementsByTagNameNS("*","player");
+			
+				if($link){
+					if( $link->item(0) ){
+						$ret['url'] = $link->item(0)->getAttribute("url");
+					}
+				}
+			
+				return $ret;
+			}
+			
+			// If the above function doesn't work (i.e. the XML is not available,
+			//    this function will be called to grab the information manually and check if a video exists by checking for thumbnails
+			function getYouTubeInfoBasic($id){
+			
+				$check = @get_headers("http://img.youtube.com/vi/".$id."/1.jpg",true);
+			
+				// Check if Thumbnail exists
+				if( strpos($check[0],"404") > 0 || strpos($check[0],"400") > 0){
+					return false;
+				}
+			
+				$ret = array();
+				$ret['thumbnails'][] = "http://img.youtube.com/vi/".$id."/1.jpg";
+				$ret['thumbnails'][] = "http://img.youtube.com/vi/".$id."/2.jpg";
+				$ret['thumbnails'][] = "http://img.youtube.com/vi/".$id."/3.jpg";
+			
+				$ret['title'] = "Watch Video";
+				$ret['url'] = "http://www.youtube.com/watch?v=".$id;
+			
+				return $ret;
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			
 			
 			$args=array( 'showposts' => '13','cat' => $data['section3_cat'] );  $my_query = new WP_Query($args);
 			if ( $my_query->have_posts()  ) { while ($my_query->have_posts()) : $my_query->the_post(); 
 			$video = get_post_meta($post->ID, 'siiimple_video', TRUE);
-			var_dump($data['section3_link']);
+			//var_dump($data['section3_link']);
 			$count++; ?>
 			
 			<?php if ($count == 1) { ?>
